21145cff7f0ad6ae80a5510ac74725d9e6b61045
sdsfg3 <jsfh3>
2026-07-28 08:19:16 +0700

    feat: register PrimeVue with Aura theme

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,6 +5,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +21,7 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, { theme: { preset: Aura } })
             .mount(el);
     },
     progress: {
